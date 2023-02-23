ef35f6f1d27b8398e58b2d7f23758f088b5b6f66
Jack Bauer <jackbauer.work@gmail.com>
2023-02-23 16:51:02 -0500

    tasklister-mini-html-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+//! Getting id from the form 
+const form = document.getElementById("create-task-form")
+//! Gettingm the id from the input 
+const input = document.getElementById("new-task-description")
+//! Getting the id from the ul aka unordered list
+const ul = document.getElementById("tasks")
+//! Getting the id from the dropdown
+const dropdown = document.getElementById("dropdown")
+
+form.addEventListener("submit", (e)=>{
+e.preventDefault()
+const li = document.createElement("li")
+ul.append(li)
+li.innerText = input.value
+li.addEventListener("click", ()=>{
+li.remove()
+})
+if(dropdown.value === "high"){
+li.style.color = "red"
+}
+if(dropdown.value === "medium"){
+li.style.color = "orange"
+}
+if(dropdown.value === "low"){
+li.style.color = "green"
+}
+})
